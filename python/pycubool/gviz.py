f8653362d9859c67ad5f27d7421ecb3adbd1395c
Pavel Alimov <42354122+Krekep@users.noreply.github.com>
2021-03-20 14:57:55 +0300

    [gviz] Add gviz to python
    - Add matrix_data_to_gviz method, which create gviz script

---
+++ python/pycubool/gviz.py
@@ -0,0 +1,32 @@
+def matrix_data_to_gviz(shape, rows, cols, **kwargs):
+    if len(rows) != len(cols):
+        raise Exception("Rows and cols arrays must have equal size")
+    result = "digraph {\n"
+    graph_name = kwargs.get("graph_name")
+    vertex_color = kwargs.get("vertex_color")
+    edge_color = kwargs.get("edge_color")
+    _base_vertex = kwargs.get("base_vertex")
+    _label = kwargs.get("label")
+    label = "" if _label is None else _label
+    base_vertex = 0 if _base_vertex is None else _base_vertex
+
+    n = shape[0]
+    m = shape[1]
+
+    if not (graph_name is None):
+        result += f'graph [label={graph_name}];\n'
+    if not (vertex_color is None):
+        result += f'node [color={vertex_color}];\n'
+    if not (vertex_color is None):
+        result += f'edge [color={edge_color}];\n'
+
+    used_vertex = [False] * n
+    for i in range(len(rows)):
+        result += f"{rows[i] + base_vertex} -> {cols[i] + base_vertex} [label={label}];\n"
+        used_vertex[rows[i]] = True
+        used_vertex[cols[i]] = True
+    for i in range(n):
+        if not used_vertex[i]:
+            result += f"{i + base_vertex};\n"
+    result += "}\n"
+    return result
